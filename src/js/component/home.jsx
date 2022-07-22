351063f018ac9fbe7972b3dd731c57acb49957ea
andresguerrah <andres.guerrah@gmail.com>
2022-07-22 18:19:13 +0000

    primer avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, addTarea]=useState(["tarea1","tarea2","Tarea3"]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="content">
+			<h1>To Do List!</h1><button>Agregar</button>
+			<input type={"text"}/>
+			<div>{tarea.map((valor, indice, arr)=>{
+				return <li key={indice}>{valor}
+				<button>X</button>
+				</li>
+		})}</div>
+		<h5>Tareas Restantes: {tarea.length}</h5>
 		</div>
 	);
 };
